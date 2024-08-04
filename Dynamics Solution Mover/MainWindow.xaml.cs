7b1c72b54aefa6fffdd00951b53b4eb7be012a95
Jakub Panczyszyn <jakub.panczyszyn.2000@gmail.com>
2024-08-04 18:55:46 +0100

    Created initial files and small first functions

--- Dynamics Solution Mover/MainWindow.xaml.cs
+++ Dynamics Solution Mover/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,7 +20,60 @@ namespace Dynamics_Solution_Mover
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo startInfo = new()
+            {
+                FileName = "pac",                
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            try
+            {
+                var proc = Process.Start(startInfo);
+
+                ArgumentNullException.ThrowIfNull(proc);
+
+                string output = proc.StandardOutput.ReadToEnd();
+                proc.WaitForExit();
+
+                Output.Text = output;
+            }
+            catch (Win32Exception ex)
+            {
+                PACInstaller.Visibility = Visibility.Visible;
+                Output.Text = ex.Message;
+            }
+            catch (ArgumentNullException ex) 
+            {
+                Output.Text = ex.Message;   
+            }            
+        }
+
+        private void PACInstaller_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessStartInfo startInfo = new()
+            {
+                FileName = "dotnet",
+                Arguments = "tool install --global Microsoft.PowerApps.CLI.Tool",
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var proc = Process.Start(startInfo);
+
+            ArgumentNullException.ThrowIfNull(proc);
+
+            string output = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+
+            Output.Text = output;
         }
     }
 }
