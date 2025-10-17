ff303e7e302e58564ffeea35aa1fac33b990e840
Jakub Panczyszyn <jakub.panczyszyn.2000@gmail.com>
2025-10-17 06:12:07 +0100

    streamed lined the functions to run pac

--- Dynamics Solution Mover/PacCommands.cs
+++ Dynamics Solution Mover/PacCommands.cs
@@ -12,28 +12,9 @@ namespace Dynamics_Solution_Mover
 {
     internal static class PacCommands
     {
-        internal static string CheckIfPac()
-        {
-            ProcessStartInfo checkIfPacExists = new()
-            {
-                FileName = "pac",
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false
-            };
-
-            var proc = Process.Start(checkIfPacExists);
-            ArgumentNullException.ThrowIfNull(proc);
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-
-            return output;
-        }
-
         internal static async Task<string> CheckIfPacAsync()
-        {
-            return await Task.Run(() => CheckIfPac());
+        {            
+            return await PacRunner.RunPacCommandAsync("");
         }
 
         internal static string InstallPac()
@@ -61,105 +42,53 @@ namespace Dynamics_Solution_Mover
             return await Task.Run(() => InstallPac());
         }
 
-        internal static string PacAuthList()
-        {
-            ProcessStartInfo authList = new()
-            {
-                FileName = "pac",
-                Arguments = "auth list",
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false
-            };
-
-            var proc = Process.Start(authList);
-            ArgumentNullException.ThrowIfNull(proc);
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-
-            return output;
-        }
-
         internal static async Task<string> PacAuthListAsync()
         {
-            return await Task.Run(() => PacAuthList());
-        }
-
-
-        internal static string PacAuthCreate()
-        {
-            ProcessStartInfo authCreate = new()
-            {
-                FileName = "pac",
-                Arguments = "auth create",
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false
-            };
-
-            var proc = Process.Start(authCreate);
-            ArgumentNullException.ThrowIfNull(proc);
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-
-            return output;
+            return await PacRunner.RunPacCommandAsync("auth list");
         }
 
         internal static async Task<string> PacAuthCreateAsync()
         {
-            return await Task.Run(() => PacAuthCreate());
-        }
-
-        internal static string PacAuthSelect(int index)
-        {
-            ProcessStartInfo authSelect = new()
-            {
-                FileName = "pac",
-                Arguments = $"auth select -i {index}",
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false
-            };
-
-            var proc = Process.Start(authSelect);
-            ArgumentNullException.ThrowIfNull(proc);
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-
-            return output;
+            return await PacRunner.RunPacCommandAsync("auth create");
         }
 
         internal static async Task<string> PacAuthSelectAsync(int index)
         {
-            return await Task.Run(() => PacAuthSelect(index));
-        }
-
-        internal static string PacAuthWho()
-        {
-            ProcessStartInfo authWho = new()
-            {
-                FileName = "pac",
-                Arguments = $"auth who",
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false
-            };
-
-            var proc = Process.Start(authWho);
-            ArgumentNullException.ThrowIfNull(proc);
-            string output = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-
-            return output;
+            return await PacRunner.RunPacCommandAsync($"auth select -i {index}");
         }
 
         internal static async Task<string> PacAuthWhoAsync()
         {
-            return await Task.Run(() => PacAuthWho());
+            return await PacRunner.RunPacCommandAsync($"auth who");
+        }
+    }
+
+    internal static class PacRunner
+    {
+        internal static async Task<string> RunPacCommandAsync(string arguments)
+        {
+            ProcessStartInfo command = new()
+            {
+                FileName = "pac",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(command);
+            ArgumentNullException.ThrowIfNull(process);
+
+            string output = await process.StandardOutput.ReadToEndAsync();
+            string error = await process.StandardError.ReadToEndAsync();
+
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+                throw new Exception($"PAC command failed: {error}");
+
+            return output;
         }
     }
 }
